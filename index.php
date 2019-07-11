146cdf0d92a15a1cb7cb337ae534d2044696eadc
romario <romariomk700@gmail.com>
2019-07-10 21:34:51 -0300

    resolvir pq nao tinha salvo

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
 <<<<<<< HEAD
-echo "OLA MUNDO DE QUEM>>> !";
-echo "DELE HORA>>> !";
-=======
-echo "OLA MUNDO de DEUS>> !";
-echo "OLA MUNDO de DEUS>> !";
->>>>>>> 0f82ce0bc76b33065174c07f8f73f877a2c0f1af
+echo "OLA MUNDO DE QUEM ? DEUS>>> !";
+echo "DELE HORA , CRIADOR DEUS>>> !";
+
 ?>
